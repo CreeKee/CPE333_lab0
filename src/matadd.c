9b1568c2578997831baaf156665f8e7469700c82
Seth <sethdkiefer@gmail.com>
2023-04-09 17:36:30 -0700

    simple matrix addition

--- src/matadd.c
+++ src/matadd.c
@@ -1,20 +1,25 @@
 
-#include "dataset.h"
+//#include "dataset.h"
 #include <stddef.h>
+#include <stdio.h>
 
 // NxN matrices 
-void matadd(int N, int M, const data_t A[], const data_t B[], data_t C[])
+void matadd(int N, int M, const int A[], const int B[], int C[])
 {
-  int i, j, k;
+  int i, j;
  
-  for (i = 0; i < M; i++) {
-    for (j = 0; j < N; j++) {
-      C[i + j*N] = A[j*N + k] + B[j*N + i];
+  for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++) {
+      C[j*N + i] = A[j*N + i] + B[j*N + i];
+      putchar(C[j*N+i]+'0');
     }
   }
 }
-void main()
+int main()
 {
-	static data_t results_data[ARRAY_SIZE];
-	matadd(DIM_SIZE, input1_data, input2_data, results_data);
+  static int input1_data[4] = {0, 1, 2, 3};
+  static int input2_data[4] = {4, 5, 6, 7} ;
+	static int results_data[4];
+	matadd(2, 2, input1_data, input2_data, results_data);
+  return 0;
 }
