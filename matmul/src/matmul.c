f291daebef5ebc0260f05edeb120ec46669e74cf
Seth <sethdkiefer@gmail.com>
2023-04-11 22:49:40 -0700

    increased mult matrices size

--- matmul/src/matmul.c
+++ matmul/src/matmul.c
@@ -1,10 +1,12 @@
 
 
 
-#define DIM_SIZE 3
+#define DIM_SIZE 5
 #define ARRAY_SIZE DIM_SIZE*DIM_SIZE
-static int input1_data[ARRAY_SIZE] = {7, 20, 23, 36, 46, 78, 87, 96, 97};
-static int input2_data[ARRAY_SIZE] = {0, 18, 22, 23, 35, 39, 40, 42, 6};
+
+static int input1_data[ARRAY_SIZE] = {0, 1, 2, 3, 6, 8, 10, 11, 14, 18, 26, 36, 42, 51, 55, 59, 66, 71, 82, 84, 89, 90, 92, 95, 96};
+static int input2_data[ARRAY_SIZE] = {9, 10, 13, 17, 18, 20, 22, 25, 27, 36, 37, 43, 44, 45, 51, 53, 69, 70, 84, 86, 87, 88, 94, 96, 97};
+
 
 // NxN matrices 
 void matmul(int N,  const int A[], const int B[], int C[])
