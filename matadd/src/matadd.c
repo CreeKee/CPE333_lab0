7a09c8768ca9b588101ac4ae56450061e0a079f8
Seth Kiefer <70313470+CreeKee@users.noreply.github.com>
2023-05-29 17:41:03 -0700

    Update matadd.c

--- matadd/src/matadd.c
+++ matadd/src/matadd.c
@@ -2,8 +2,8 @@
 #define DIM_SIZE 3
 #define ARRAY_SIZE DIM_SIZE*DIM_SIZE
 
-static int input1_data[ARRAY_SIZE] = {353, 533, 155, 955, 213, 952, 522, 146, 664}
-static int input2_data[ARRAY_SIZE] = {558, 904, 879, 641, 130, 324, 575, 157, 326}
+static int input1_data[ARRAY_SIZE] = {353, 533, 155, 955, 213, 952, 522, 146, 664};
+static int input2_data[ARRAY_SIZE] = {558, 904, 879, 641, 130, 324, 575, 157, 326};
 
 // NxN matrices 
 void matadd(int N, int M, const int A[], const int B[], int C[])
